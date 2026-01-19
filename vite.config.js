a45ac17b5098a2adfc443efda687361f3d8cbe8c
fortunecloudtraining <fortunecloudtraining@gmail.com>
2026-01-19 17:20:46 +0530

    asdsa

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/hospital_ui/", // ✅ REQUIRED for GitHub Pages
+  base: "/mern_hospitalui/"
 });
